030a5c1e0771866c7da46ff064398b2769fedd54
StreakInTheSky <streakinthesky@gmail.com>
2017-05-20 01:15:19 -0700

    Refactor to use with app

--- src/routes/system.js
+++ src/routes/system.js
@@ -1,9 +1,5 @@
-const SystemController = require('../controllers/system_controller')
+const UserController = require('../controllers/user_controller')
 
 module.exports = (app) => {
-  app.get('/api/system', SystemController.getAll)
-  app.get('/api/system/:id', SystemController.getOne)
-  app.post('/api/system', SystemController.create)
-  app.put('/api/system/:id', SystemController.edit)
-  app.delete('/api/system/:id', SystemController.delete)
+  app.post('/api/user', UserController.create)
 }
