f914b92579d872739f68e2a1f33483a88af60d1a
StreakInTheSky <streakinthesky@gmail.com>
2017-05-20 01:13:40 -0700

    Replace system_controller with user_controller

---
+++ src/controllers/user_controller.js
@@ -0,0 +1,32 @@
+const User = require('../models/user')
+const utils = require('../utilities/user-utilities')
+
+module.exports = {
+  create(req, res, next) {
+    const requiredFields = ['username', 'email', 'password', 'passwordConfirm']
+
+    requiredFields.forEach(field => {
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message)
+        return res.status(400).send(message)
+      }
+      return null
+    })
+
+    if (!utils.passwordChecker(req.body.password, req.body.passwordConfirm)) {
+      return res.status(400).send('passwords did not match')
+    }
+
+    const user = new User(requiredFields)
+    user.save()
+      .then((savedUser) => {
+        User.findById(savedUser._id)
+          .then(() => {
+            res.status(201).send(user.apiRepr())
+          })
+          .catch(next)
+      })
+      .catch(next)
+  }
+}
