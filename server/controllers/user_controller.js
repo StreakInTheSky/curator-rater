92d710a9993274e6d394a11c4b7df7a389c9dd87
StreakInTheSky <streakinthesky@gmail.com>
2017-05-25 00:16:40 -0700

    Add authentication and add field checkers

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,8 +1,12 @@
 const User = require('../models/user')
-const utils = require('../utilities/user_utilities')
+// const utils = require('../utilities/user_utilities')
 
 module.exports = {
   create(req, res, next) {
+    if (!req.body) {
+      return res.status(400).send('No request body')
+    }
+
     const requiredFields = ['username', 'email', 'password', 'passwordConfirm']
 
     requiredFields.forEach(field => {
@@ -14,24 +18,71 @@ module.exports = {
       return null
     })
 
-    if (!utils.passwordChecker(req.body.password, req.body.passwordConfirm)) {
-      return res.status(400).send('passwords did not match')
+    let { username, email, password, passwordConfirm } = req.body
+
+    if (typeof username !== 'string') {
+      return res.status(422).send('Incorrect field type: username')
     }
 
-    const userProps = {
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
+    username = username.trim()
+
+    if (username === '') {
+      return res.status(422).send('Incorrect field length: username')
     }
-    const user = new User(userProps)
-    user.save()
-      .then((savedUser) => {
-        User.findById(savedUser._id)
-          .then(() => {
-            res.status(201).send(user.apiRepr())
-          })
-          .catch(next)
+
+    if (typeof email !== 'string') {
+      return res.status(422).send('Incorrect field type: email')
+    }
+
+    email = email.trim()
+
+    if (email === '') {
+      return res.status(422).send('Incorrect field length: email')
+    }
+
+    if (typeof password !== 'string') {
+      return res.status(422).send('Incorrect field type: password')
+    }
+
+    password = password.trim()
+    passwordConfirm = passwordConfirm.trim()
+
+    if (password === '') {
+      return res.status(422).send('Incorrect field length: password')
+    }
+
+    if (!(password === passwordConfirm)) {
+      return res.status(422).send('passwords do not match')
+    }
+
+    // check for existing user
+    return User
+      .find({ username })
+      .count()
+      .exec()
+      .then(count => {
+        if (count > 0) {
+          return res.status(422).send('username already taken')
+        }
+        // if no existing user, hash password
+        return User.hashPassword(password)
       })
+      .then(hash => {
+        return User.create({
+          username: username,
+          email: email,
+          password: hash
+        })
+      })
+      .then(user => {
+        return res.status(201).json(user.apiRepr())
+      })
+      .catch(next)
+  },
+  getAll(req, res, next) {
+    return User
+      .find({})
+      .then(users => res.status(200).json(users.map(user => user.apiRepr())))
       .catch(next)
   }
 }
