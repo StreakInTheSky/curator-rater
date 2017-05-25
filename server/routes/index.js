62dc3b87cbe1dad51902d623e91e52dc74694bd1
StreakInTheSky <streakinthesky@gmail.com>
2017-05-25 00:18:48 -0700

    Rename routes file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 const UserController = require('../controllers/user_controller')
 
 module.exports = (app) => {
+  // User routes
+  app.get('/api/user', UserController.getAll)
   app.post('/api/user', UserController.create)
 }
