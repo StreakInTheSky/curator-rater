3f0694852d217d44a5f58009bd50d2d8861ef2f3
StreakInTheSky <streakinthesky@gmail.com>
2017-05-25 00:18:17 -0700

    Add some utilities (will change later)

--- server/utilities/user_utilities.js
+++ server/utilities/user_utilities.js
@@ -1,5 +1,30 @@
+const fieldChecker(arr) {
+  const requiredFields = ['username', 'email', 'password', 'passwordConfirm']
+
+  requiredFields.forEach(field => {
+    if (!(field in arr)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message)
+      return res.status(400).send(message)
+    }
+    return null
+  })
+}
+
 const passwordChecker(password, passwordConfirm) {
   return password === passwordConfirm
 }
 
-module.exports = { passwordChecker }
+const usernameChecker(username) {
+  return User
+  .find({username})
+  .count()
+  .exec()
+  .then(count => {
+    if (count > 0) {
+      return true
+      })
+    }
+}
+
+module.exports = { fieldChecker, passwordChecker, usernameChecker }
