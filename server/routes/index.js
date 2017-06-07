00a6d9894e165e933c2ae0b81ba64d674a093dd4
StreakInTheSky <streakinthesky@gmail.com>
2017-06-07 01:54:13 -0700

    Create routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,40 @@
 const UserController = require('../controllers/user_controller')
+const FetchController = require('../controllers/fetch_controller')
+// const GalleryController = require('../controllers/gallery_controller')
+// const PhotoController = require('../controllers/photo_controller')
+// const NotificationController = require('../controllers/notification_controller')
 
 module.exports = (app) => {
-  const apiUrl = '/api/user'
   // User routes
-  app.get(apiUrl, UserController.getAll)
-  app.get(`${apiUrl}/:username`, UserController.getOne)
-  app.post(apiUrl, UserController.create)
-  app.post(`${apiUrl}/follow`, UserController.follow)
-  app.post(`${apiUrl}/unfollow`, UserController.unfollow)
-  app.put(`${apiUrl}/:userid`, UserController.update)
-  app.delete(`${apiUrl}/:userid`, UserController.delete)
+  const apiUser = '/api/user'
+  app.get(apiUser, UserController.getAll)
+  app.get(`${apiUser}/:username`, UserController.getOne)
+  app.post(apiUser, UserController.create)
+  app.post(`${apiUser}/follow`, UserController.follow)
+  app.post(`${apiUser}/unfollow`, UserController.unfollow)
+  app.put(`${apiUser}/:userid`, UserController.update)
+  app.delete(`${apiUser}/:userid`, UserController.delete)
+
+  const apiFetch = '/api/fetch'
+  app.get(`${apiFetch}/image-url`, FetchController.fetchUrl)
+
+  // // Gallery routes
+  // const apiGallery = '/api/gallery'
+  // app.get(apiGallery, GalleryController.get)
+  // app.get(`${apiGallery}/:galleryId`, GalleryController.getOne)
+  // app.post(apiGallery, GalleryController.create)
+  // app.post(`${apiGallery}/:galleryId/favorite`, GalleryController.addFavorite)
+  // app.put(`${apiGallery}/:galleryId`, GalleryController.update)
+  // app.delete(`${apiGallery}/:galleryId`, GalleryController.delete)
+  //
+  // // Photo routes
+  // const apiPhoto = '/api/photo'
+  // app.get(apiPhoto, PhotoController.get)
+  // app.get(`${apiPhoto}/:photoId`, PhotoController.getOne)
+  // app.post(`${apiPhoto}/:photoId/vote`, PhotoController.vote)
+  // app.delete(`${apiPhoto}/:photoId`, PhotoController.delete)
+  //
+  // // Notification routes
+  // const apiNotification = '/api/notification'
+  // app.put(`${apiNotification}/:notificationId`, NotificationController.markRead)
 }
