db1fd78cd8b22079d0c1cc15b6c721409bfe182d
StreakInTheSky <streakinthesky@gmail.com>
2017-06-30 19:28:44 -0700

    rename test db name

--- server/config.js
+++ server/config.js
@@ -3,5 +3,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                       'mongodb://localhost/curator-rator'
 exports.DATABASE_URL_TEST = process.env.DATABASE_URL_TEST ||
                        global.DATABASE_URL_TEST ||
-                      'mongodb://localhost/curator-rator_test'
+                      'mongodb://localhost/curator-rator-test'
 exports.PORT = process.env.PORT || 3000
