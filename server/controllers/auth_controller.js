5946aeab1b3647e8528428449cddf43307d002ae
StreakInTheSky <streakinthesky@gmail.com>
2017-10-30 12:50:28 -0700

    change token setup

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -3,13 +3,15 @@ const jwt = require('jsonwebtoken')
 const config = require('../config')
 
 const createAuthToken = user => {
-  return jwt.sign({
-    user: { username: user.username }
-  }, config.JWT_SECRET, {
-    subject: user.username,
-    expiresIn: config.JWT_EXPIRY,
-    algorithm: 'HS256'
-  })
+  return jwt.sign(
+    { user },
+    config.JWT_SECRET,
+    {
+      subject: user.username,
+      expiresIn: config.JWT_EXPIRY,
+      algorithm: 'HS256'
+    }
+  )
 }
 
 module.exports = {
