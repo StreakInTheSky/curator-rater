0441aef742aa81e3c5e93a994ba42f5a494db66f
StreakInTheSky <streakinthesky@gmail.com>
2017-11-02 05:33:19 -0700

    fix name of local db names

--- server/config.js
+++ server/config.js
@@ -1,9 +1,9 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/curator-rator'
+                      'mongodb://localhost/curator-rater'
 exports.DATABASE_URL_TEST = process.env.DATABASE_URL_TEST ||
                        global.DATABASE_URL_TEST ||
-                      'mongodb://localhost/curator-rator-test'
+                      'mongodb://localhost/curator-rater-test'
 exports.PORT = process.env.PORT || 8080
 exports.JWT_SECRET = process.env.JWT_SECRET
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'
